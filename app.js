97a448eb381e9c9336bd94283fdb21bc73068d19
zor-cell <philippemanu.zorman@gmail.com>
2022-12-29 01:52:32 +0100

    creating user interface with react

--- app.js
+++ app.js
@@ -1,36 +1,20 @@
-const ROWS = 6;
-const COLS = 7;
-
-let width = 100 / ROWS;
-let height = 100 / COLS;
-
-let board = setBoard();
-console.log(board)
-
-function setBoard() {
-    let curBoard = [];
-
-    for(let i = 0;i < ROWS;i++) {
-        curBoard[i] = [];
-        for(let j = 0;j < COLS;j++) {
-            curBoard[i].push(0);
-        }
-    }
-
-    return curBoard;
-}
+const SETTINGS = {
+    rows: 6,
+    cols: 7,
+    player: 2,
+    depth: 3,
+  };
 
 function App() {
-    const [runs, setRuns] = React.useState(100000);
-
-    function onCellClick(event) {
-        console.log(event.target.className);
-        event.stopPropagation();
-    }
-
-    function onRunsChange(event) {
-        setRuns(Number(event.target.value));
-    }
+    //2d array as game board
+    const [board, setBoard] = React.useState(
+        Array.from({length: SETTINGS.rows}, () => Array.from({length: SETTINGS.cols}, () => 0))
+    );
+    //amount of pieces in each column
+    const [height, setHeight] = React.useState(Array.from({length: SETTINGS.cols}, () => 0));
+    //current player to move
+    const [player, setPlayer] = React.useState(SETTINGS.player);
+    
 
     function handleSubmit(event) {
         document.getElementById("result").innerHTML = "Computing result...";
@@ -48,39 +32,74 @@ function App() {
         });
     }
 
+    //check if given move is possible
+    function validMove(col) {
+        return height[col] < SETTINGS.rows;
+    }
+
+    //set piece in given column if possible
+    function dropPiece(columnObject) {
+        let col = columnObject.columnIndex;
+
+        if(validMove(col)) {
+            let row = SETTINGS.rows - 1 - height[col];
+
+            //set cell at [row, col] to player
+            let newBoard = board.map((curRow, i) => {
+                if(i === row) {
+                    return curRow.map((cell, j) => {
+                        if(j === col) return player;
+                        else return cell;
+                    });
+                } else return curRow;
+            });
+            setBoard(newBoard);
+
+            //increase height at given column by 1
+            let newHeight = height.map((h, i) => {
+                if(i === col) return h + 1;
+                else return h;
+            });
+            setHeight(newHeight);
+            
+            //switch current player
+            if(player === 1) setPlayer(2)
+            else setPlayer(1);
+
+            console.log(board);
+        }
+    }
+
+    function setCellColor(cellValue) {
+        if(cellValue === 1) return 'player1';
+        else if(cellValue === 2) return 'player2';
+
+        return 'empty';
+    }
+
     return (
-        <div>
-            <h1>My Title</h1>
-            <div>
-                <form id="parameters" onSubmit={handleSubmit}>
-                    <span>Runs</span>
-                    <input type="number" value={runs} onChange={onRunsChange}></input>
-
-                    <input type="submit" value="Run"></input>
-                </form>
-            </div>
-
-            <h2>Result</h2>
-            <h3 id="result">No Result</h3>
+        <React.Fragment>
+            <h1>Connect 4</h1>
 
             <table className="board">
-                <thead></thead>
                 <tbody>
-                    {board.map((row, rowKey) => {
+                    {board.map((row, rowIndex) => {
                         return (
-                            <tr key={rowKey}>
-                                {row.map((cell, cellKey) => {
-                                    return (
-                                        <td onClick={onCellClick} style={{width: 100/6 + "%", height: 25 + "%"}} className={rowKey.toString() + cellKey.toString()} key={cellKey}>{cell}</td>
-                                    )
+                            <tr key={rowIndex} className="row">
+                                {row.map((cell, columnIndex) => {
+                                    return <td key={columnIndex} className="cell"  onClick={() => dropPiece({columnIndex})}>
+                                        <div className={`piece ${setCellColor(cell)}`}></div>
+                                    </td>
                                 })}
                             </tr>
                         )
                     })}
                 </tbody>
             </table>
-            
-        </div>
+
+            <h2>Result</h2>
+            <h3 id="result">No Result</h3>
+        </React.Fragment>
     );
 }
 
